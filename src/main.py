b733ab9c54ed8354fc610698f19e397ca693808a
Kelly <kelly.degannes@gmail.com>
2023-10-21 22:32:42 +1100

    Added menu features to main.py

---
+++ src/main.py
@@ -0,0 +1,73 @@
+'''
+1. Create login page
+    - Enter email address / password
+    - If incorrect, give message, allow repeat attempts
+    - If correct, display welcome message
+
+2. Create menu
+    - Search for titles, by name, genre etc
+    - Check for current rentals
+    - Rent title
+    - Return title
+    - Confirmation
+
+3. Class for users, contains their rental history, current rentals (recommendations?)
+
+4. Class for titles (data required: title, genre, actors, year, runtime, rating, plot)
+
+5. Main will be responsible for: 
+    - requesting username/password
+    - displaying the menu
+    - adding users
+    - renting/returning titles
+    
+'''
+
+
+
+# Main
+
+# Display menu
+def display_main_menu():
+    print('Welcome to K-Star Video - the home of all the latest movies')
+    print('*************************************************************\n')
+    print('1. Search for a title')
+    print('2. Your account')
+    print('3. Return items')
+    print('4. Exit')
+
+def display_search_menu():
+    print('1. Search by title')
+    print('2. Search by actors e.g. "Tom Hanks or Julia Roberts"')
+    print('3. Search by genre')
+
+def display_genre_menu():
+    print('1. Comedy')
+    print('2. Romance')
+    print('3. Drama')
+    print('4. Musical')
+    print('5. Kids')
+    print('6. Action')
+    print('7. Sci-fi')
+    print('8. Horror')
+    print('9. Documentary')
+    print('10. Biopic')
+    print('11. TV Shows')
+
+user_response = ''
+
+while user_response != '4':
+    display_main_menu()
+
+    match user_response:
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case _:
+            # raise exception
+
