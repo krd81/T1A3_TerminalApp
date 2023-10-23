fb72177e0239bc30a82379ae391b629eacdb8984
Kelly <kelly.degannes@gmail.com>
2023-10-23 21:25:32 +1100

    Movies now displaying in groups of 10

--- main.py
+++ main.py
@@ -22,7 +22,7 @@
     - renting/returning titles
     
 '''
-import json
+import json, math
 class User:
     def __init__(self, id, email, first, last, company, created_at, country, password) -> None:
         self.id = id
@@ -165,23 +165,76 @@ with open ('user_list.json') as f:
 
 with open ('movies.json') as f:
     movies = json.load(f)
+    # movies = movies.reverse()
 
+matching_movies = []
+
+
+def get_user_input(prompt):
+    input_value = input(prompt)
+    print('\n')
+    return input_value
+
+
+def remainder_check(numerator, divisor):
+    result = 0
+    if(numerator % divisor) > 0:
+        result = 1
+    return result
 
 # Need to know the search term (title, actor, genre??) - lambda? comprehension?
 # Show a maximum of 10 results? Option to view next results if >10
 # Need to be able to select a title once the list is populated
 def search_movies():
     search_title = get_user_input('Enter a title: ')
-    matching_movies = []
-    i = 1
+    global matching_movies
+    
     for movie in movies:       
         if (search_title in movie.get('title')):
-            current_movie = Movie(movie.get('title'), movie.get('year'), movie.get('cast'), movie.get('genres'), movie.get('href'), movie.get('extract'), movie.get('thumbnail'), movie.get('thumbnail_width'), movie.get('thumbnail_height'))
-            print(f'{i}: {current_movie.get_title()} ({current_movie.get_year()})')
-            current_movie.cast_shortlist() # prints cast names
-            print('\n')
+            current_movie = Movie(movie.get('title'), movie.get('year'), movie.get('cast'), movie.get('genres'), movie.get('href'), movie.get('extract'), movie.get('thumbnail'), movie.get('thumbnail_width'), movie.get('thumbnail_height'))            
             matching_movies.append(current_movie)
-            i += 1
+
+    num_movies = len(matching_movies)
+    
+    i = 0
+    '''
+    i = 0 : movie 1 - 10
+    i = 1 : movie 11 - 20
+    i = 2 : movie 21 - 30
+    i = 3 : movie 31 - 33 (31:)
+    '''
+    # 'pages' to cycle when displaying movies on screen
+    pages = (num_movies // 10) + remainder_check(num_movies, 10)
+    while (i < pages):
+        if (i == 0): # First page
+            display_movies(0, 10)
+            if (pages > 1):
+                print("More...")
+                get_user_input("Press enter to continue")
+        elif (i == pages-1): # Last page <--- THIS IS CAUSING AN ERROR!
+            display_movies(i*10, -1)
+        else: # All other pages
+            display_movies(i*10, (i+1)*10)
+            print("More...")
+            get_user_input("Press enter to continue")
+        i += 1
+
+
+# Method to display 10 movies at a time
+def display_movies(from_index, to_index):
+    index = from_index
+    count = 1
+    while index < to_index:
+        # For element 0 - 9 [10 - 19, 20 - 29 etc], print movie info
+        print(f'{count}: {matching_movies[index].get_title()}: ({matching_movies[index].get_year()})')
+        matching_movies[index].cast_shortlist() # prints cast names
+        print('\n')
+        index += 1
+        count += 1
+        
+
+
+
 
 
 def menu_control():
@@ -224,11 +277,6 @@ def password_check(password):
         result = True
     return result
 
-
-def get_user_input(prompt):
-    input_value = input(prompt)
-    print('\n')
-    return input_value
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+import math
 cast = ['Leonardo DiCaprio', 'Ken Watanabe', 'Joseph Gordon-Levitt', 'Marion Cotillard', 'Elliot Page', 'Tom Hardy', 'Cillian Murphy', 'Tom Berenger', 'Michael Caine', 'Dileep Rao', 'Pete Postlethwaite', 'Lukas Haas', 'Tim Kelleher', 'Talulah Riley', 'Michael Gaston', 'Felix Scott', 'Andrew Pleavin', 'Hélène Cardona', 'Matt Vogel']
 cast2 = [
         "Tom Hanks",
@@ -28,3 +28,11 @@ while i < num_cast:
 if len(cast) > 4 :
     print('...')
 
+num_movies = 33
+answer = ((num_movies // 10) + math.ceil(num_movies % 10))
+first_num = (num_movies // 10)
+second_num = math.ceil(num_movies % 10)
+
+# 3 + 1 = 4
+
+print(f'the answer is {answer}. This is {first_num} + {second_num}')
